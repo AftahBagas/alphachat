99da4139baf2d848ce01d1af2fac365a146327b8
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-22 19:56:02 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,1 @@
-import os
-from AnonymousBot import getMyBot
-
-def homepage():
-    return "Running", 200
-
-port = int(os.environ.get('PORT', 33507))
-
-myBot = getMyBot(HOSTNAME=r"https://alphachatz.herokuapp.com")
-
-myBot.getFlask().route("/", methods=['GET'], endpoint="routeHomepage")(homepage)
-
-myBot.run(host='0.0.0.0', port=port)
+python-3.8.5
